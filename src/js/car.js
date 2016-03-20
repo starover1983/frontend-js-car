af4d8d7116122fdfae12b7c4f07c1ac763308dbf
starover1983 <A_lexei@mail.ru>
2016-03-20 17:49:26 +0200

    Added files via upload

--- src/js/car.js
+++ src/js/car.js
@@ -1,1 +1,35 @@
-﻿'use strict';
+'use strict';
+var car = {
+    color: "white",
+    dours: 4,
+    speed: 0,
+    defaultSpeed: 60,
+    maxSpeed: 100,
+    seats: 4,
+    passengers: [],
+    put: function put() {
+        if (this.passengers.length < 1) {
+            this.passengers.push('Driver');
+        } else if (this.passengers.length < car.seats) {
+            this.passengers.push(true);
+        }
+    },
+
+
+    land: function land() {
+        this.passengers.pop();
+    },
+
+    drive: function drive(newSpeed) {
+
+        if (newSpeed === undefined && this.passengers[0]) {
+            this.speed = this.defaultSpeed;
+        } else if (newSpeed > this.maxSpeed && this.passengers[0]) {
+            this.speed = this.maxSpeed
+        } else if (!this.passengers[0]) {
+            return
+        } else
+            return this.speed = newSpeed;
+
+    },
+}
